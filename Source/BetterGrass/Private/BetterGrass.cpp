845a09c172cb7ef82f66c3d635ee6d11de2bf311
SpaceGameDev568 <space@spacegamedev.com>
2023-07-03 12:54:28 -0400

    Initial Commit

---
+++ Source/BetterGrass/Private/BetterGrass.cpp
@@ -0,0 +1,14 @@
+﻿#include "BetterGrass.h"
+#include "Interfaces/IPluginManager.h"
+
+#define LOCTEXT_NAMESPACE "FBetterGrassModule"
+
+void FBetterGrassModule::StartupModule()
+{
+}
+
+void FBetterGrassModule::ShutdownModule()
+{
+}
+
+IMPLEMENT_MODULE(FBetterGrassModule, BetterGrass)
---
+++ Source/BetterGrass/Public/BetterGrass.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogBetterGrass, Verbose, All);
+
+class FBetterGrassModule : public IModuleInterface
+{
+
+	
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
